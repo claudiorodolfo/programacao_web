40a29f1578b429847cfb67586ed7ed320f671e2a
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-02-29 21:34:35 -0300

    Update editar.php

--- aconchego/visao/usuario/editar.php
+++ aconchego/visao/usuario/editar.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="pt-br">
   <head>
-    <!-- Required meta tags -->
+    <!-- Requird meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">   
     <!-- JQuery JS -->  
